68ae781a0fb81889e3211d8926d044552b471e9a
Cross Leonard <cleonardross11@gmail.com>
2026-04-18 22:33:45 +0100

    added README

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 import pyperclip
 from manga_ocr import MangaOcr
 import sct
-from clearboxfeature import select_box
-
-
-
+import box_selector
 
 mocr = MangaOcr()
 box = select_box()
 
-if box is None:
-	print("No selection made.")
-	raise SystemExit(0)
-
 screenshot_path = sct.sct(box)
 text = mocr(screenshot_path)
-
-
 
 pyperclip.copy(text)
 print(text)
